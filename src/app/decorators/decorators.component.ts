f04ac105e03be4a795769750006e469a876722de
rockGithub <rock20110414@gmail.com>
2018-04-04 19:27:20 +0800

    Decorators spike

--- src/app/decorators/decorators.component.ts
+++ src/app/decorators/decorators.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { Cat } from './decorators.model';
+import { 
+  Cat, /*readonly,*/ Dog,
+  Person,
+  MySuperHero,
+} from './decorators.model';
+
+import {
+  readonly,
+  deprecate,
+} from 'core-decorators';
 
 @Component({
   selector: 'app-decorators',
@@ -15,10 +24,11 @@ export class DecoratorsComponent implements OnInit {
   		value: function desc(params) {
   			console.log(params, 'in sex function.');
   		},
-		enumerable: false,
-		configurable: true,
-		writable: true
-	};
+  		enumerable: false,
+  		configurable: true,
+  		writable: true
+  	};
+
   	const obj = {
   		name: 'rockwang',
   		age: 35
@@ -28,11 +38,46 @@ export class DecoratorsComponent implements OnInit {
   	});
   	console.log(obj);
 
-  	////////////////////////////
+  	// below is for test: configurable
   	let cat = new Cat('Tom');
-  	cat.meow = function meow() {
+  	/*cat.meow = function meow() {
   		console.log('new meow');
-  	}
+  	}*/
+  	this.decoratorReadonlyTest();
+    this.coreDecoratorTest();
+    this.classDecoratorTest();
+  }
+
+  decoratorReadonlyTest() {
+  	let descriptor: any = { 
+  		value: function val() {
+  		},
+			enumerable: false,
+			configurable: true,
+			writable: true
+		};
+
+		descriptor = readonly(Dog.prototype, 'hao', descriptor) || descriptor;
+		Object.defineProperty(Dog.prototype, 'hao', descriptor);
+  	var garfield = new Dog('Apply');
+
+  	console.log(garfield);
+		/*garfield.hao = function(){ 
+			console.log('I want lasagne!');
+		};*/
+  }
+
+  coreDecoratorTest() {
+    let captainPicard: any = new Person();
+
+    captainPicard.facepalm();
+    captainPicard.facepalmHard();
+    captainPicard.facepalmHarder();
+  }
+
+  classDecoratorTest() {
+    let superHero: any = new MySuperHero();
+    console.log(MySuperHero);
 
   }
 
--- src/app/decorators/decorators.model.ts
+++ src/app/decorators/decorators.model.ts
@@ -1,8 +1,12 @@
+import { deprecate } from 'core-decorators';
 
+// 装饰器(Decorator)是一个会被执行的表达式，她还必须再返回一个函数。
 export const readonly = (target, name, descriptor) => {
 	descriptor.writable = false;
 }
 
+// 在 property descriptor被注册到 Cat.prototype 之前，执行引擎会先执行装饰器(Decorator)：
+//装饰器和`Object.defineProperty`具有相同的参数列表，//有机会在真正的`defineProperty`之行之前做点儿事情
 export class Cat {
 	constructor(private name: string) {}
 
@@ -12,10 +16,10 @@ export class Cat {
 	}
 }
 /**
-Object.defineProperty(obj, prop, descriptor)
+Object.defineProperty(target, name, descriptor)
 参数
-obj:		要在其上定义属性的对象。
-prop:		要定义或修改的属性的名称。
+target:		要在其上定义属性的对象。
+name:		要定义或修改的属性的名称。
 descriptor: 将被定义或修改的属性描述符。
 返回值:		被传递给函数的对象。
 
@@ -40,3 +44,46 @@ set
 
 */
 
+export class Dog {
+	constructor(private name: string) {}
+
+	// @readonly
+	hao() {
+		return `${this.name} says hao`;
+	}
+}
+
+export class Person{
+	@deprecate
+	facepalm() {
+	}
+
+	@deprecate('We stopped facepalming')
+	facepalmHard() {
+	}
+
+	@deprecate('We stopped facepalming', { url: 'http://knowyourmeme.com/memes/facepalm' })
+	facepalmHarder() {
+	}
+}
+
+
+export function superhero(target){
+	target.isSuperhero = true;
+	target.power = 'flight';
+}
+/*export function superhero(isSuperhero) {
+    return function(target){
+        target.isSuperhero = isSuperhero;
+    };
+}*/
+
+@superhero
+export class MySuperHero{}
+// below line has error
+console.log(MySuperHero);
+
+// ES2016的装饰器(Decorator)在属性和类上皆可工作。
+// 装饰器可以自动获取传入的 property name 、 target object。
+// 装饰器(Decorator)里能拿到 descriptor 使得譬如:
+// 用getter替换属性获取、自动绑定等以前要很繁琐才能完成的工作变为可能。
