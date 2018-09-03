7423b696fa35e385dd37ae7aa65e381ff34ecf72
rockwang <rockwang@localhost.localdomain>
2018-09-03 17:14:17 +0800

    upgrade from ng 4.x to 8

--- src/app/rx-spike/rx-spike.component.ts
+++ src/app/rx-spike/rx-spike.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import * as Rx from 'rxjs';
+import { Observable, fromEvent, of } from 'rxjs';
+import { debounceTime, pluck, switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-rx-spike',
@@ -11,22 +12,25 @@ export class RxSpikeComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-	// const body = document.querySelector('.layout-body');
-	let body = document.body;
-	console.log('body', body);
-	let tg = document.querySelector('#tg');
+    // const body = document.querySelector('.layout-body');
+    const body = document.body;
+    console.log('body', body);
+    const tg = document.querySelector('#tg');
 
-	//for every keyup, map to current input value
-	const example = Rx.Observable.fromEvent(window, 'resize');
+    // for every keyup, map to current input value
+    const example = fromEvent(window, 'resize');
 
-	//wait .5s between keyups to emit current value
-	//throw away all other values
-	const debouncedInput = example.throttleTime(500);
+    // wait .5s between keyups to emit current value
+    // throw away all other values
+    // https://blog.csdn.net/langyalaoa/article/details/81912513
+    const debouncedInput = example.pipe(
+      debounceTime(500)
+    );
 
-	//log values
-	const subscribe = debouncedInput.subscribe(val => {
-		console.log(`Debounced Input: ${val}`);
-	});
+    // log values
+    const subscribe = debouncedInput.subscribe(val => {
+      console.log(`Debounced Input: ${val}`);
+    });
 
   }
 
